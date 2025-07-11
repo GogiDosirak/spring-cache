b9e240b08c2fe6e968ce174a8dd0b0dfe03d683d
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-11 11:39:35 +0900

    Update CacheMetricsConfig.java

--- src/main/java/com/sprint/springcache/config/CacheMetricsConfig.java
+++ src/main/java/com/sprint/springcache/config/CacheMetricsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.context.annotation.Configuration;
 
 // Prometheus로 메트릭을 scrape하기 위함
+// Prometheus를 사용하지 않더라도, /actuator/metrics/cache. 으로 지표를 확인하려면 필요!
 @Configuration
 public class CacheMetricsConfig {
   private final CacheManager cacheManager;
